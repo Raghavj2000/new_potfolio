7b3b6e7c43dbf33b88f00886c60a1be6430bbbb0
Raghavendra <RaghavendraJ@ziniosedge.com>
2025-04-03 23:53:10 +0530

    kind of navbar for mob-

--- app/page.js
+++ app/page.js
@@ -1,4 +1,12 @@
+import Navbar from "@/components/Navbar/Navbar";
 import "./home.scss";
 export default function Home() {
-  return <section id="home"></section>;
+  return (
+    <section id="home">
+      <div style={{ zIndex: "10" }}>
+        <Navbar />
+        lol
+      </div>
+    </section>
+  );
 }
--- components/Navbar/Navbar.js
+++ components/Navbar/Navbar.js
@@ -1,8 +1,47 @@
-import React from "react";
-import "./navbar.css";
+"use client";
+
+import React, { useState } from "react";
+import "./navbar.scss";
+import { Menu, X } from "lucide-react";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  const [menuOpen, setMenuOpen] = useState(false);
+  const navlinks = [
+    { name: "Home", link: "/home" },
+    { name: "About", link: "/about" },
+    { name: "Projects", link: "/projects" },
+    { name: "Contact", link: "/contact" },
+  ];
+
+  const toggleOpen = () => {
+    setMenuOpen((prev) => !prev);
+  };
+
+  return (
+    <div className="navbar_container_mob">
+      <div className="name_container">Raghav</div>
+      <div className="menu_container" onClick={toggleOpen}>
+        {menuOpen ? (
+          <X color="white" size={25} />
+        ) : (
+          <Menu color="white" size={25} />
+        )}
+      </div>
+
+      {/* Mobile Menu */}
+      <div className={`mobile_menu ${menuOpen ? "open" : ""}`}>
+        <ul>
+          {navlinks.map((nav, index) => (
+            <li key={index}>
+              <a href={nav.link} onClick={toggleOpen}>
+                {nav.name}
+              </a>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
